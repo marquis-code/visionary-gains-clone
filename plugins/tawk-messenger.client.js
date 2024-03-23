f1eede44e2f8e1726a4985e245401c6a3470e6a5
Marquis Abah <marquis.abah@shuttlers.ng>
2024-03-23 18:45:52 +0100

    updated chatbod property id

--- plugins/tawk-messenger.client.js
+++ plugins/tawk-messenger.client.js
@@ -3,7 +3,7 @@ import TawkMessengerVue from '@tawk.to/tawk-messenger-vue-2'
 
 export default function () {
   Vue.use(TawkMessengerVue, {
-    propertyId: '657315d007843602b8ffb687',
-    widgetId: '1hh4oqe0t'
+    propertyId: '65ff11dfa0c6737bd123f69e',
+    widgetId: '1hpm637au'
   })
 }
