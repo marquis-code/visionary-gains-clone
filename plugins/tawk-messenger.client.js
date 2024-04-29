dcd6b9090b941c5a42b324d3de40cd6c45f3366f
Marquis Abah <marquis.abah@shuttlers.ng>
2024-04-29 11:06:59 +0100

    updated implementation

--- plugins/tawk-messenger.client.js
+++ plugins/tawk-messenger.client.js
@@ -3,7 +3,7 @@ import TawkMessengerVue from '@tawk.to/tawk-messenger-vue-2'
 
 export default function () {
   Vue.use(TawkMessengerVue, {
-    propertyId: '65ff11dfa0c6737bd123f69e',
-    widgetId: '1hpm637au'
+    propertyId: '662f6fdba0c6737bd132164a',
+    widgetId: '1hskkpsmj'
   })
 }
